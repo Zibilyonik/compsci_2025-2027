73d898739a233690b5fb69add800a7bec28efef3
SenyaKT <topfloort2019@gmail.com>
2025-09-25 11:10:56 +0200

    create new finle for loop

---
+++ Practice unit 1/for_loop.py
@@ -0,0 +1,14 @@
+pizza=100
+for pizza in range (20+1):
+    print(f"loop number {pizza}")
+
+
+
+burger=1011
+range_number=int(input("how many burgers will you eat?"))
+if range_number <= 1011 and range_number > 0 :
+    for burger in range (range_number + 1):
+        print(f"you can eat {burger} burgers")
+
+
+else: print("you cant eat this number of burgers")
