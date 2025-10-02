cc48939fd84657780780f3299ea7f849ebfbfc22
ChaosIncarnate7 <natalka.chwalisz@gmail.com>
2025-10-02 11:13:01 +0200

    Assignment (loops)

---
+++ Loops.py
@@ -0,0 +1,15 @@
+user_count = int(input(How many iterations))
+for x in range (1, user_count):
+    if x % 2 == 0
+        print(f"number {x} is divisible by 2")
+    else:
+        print(f"number {x} is not divisible by 2")
+
+user_while_count = int(input("what's the max value?"))
+counter = 0
+while counter <= user_while_count:
+    if counter % 2 == 0
+        print(f"number {counter} is divisible by 2")
+    else:
+        print(f"number {counter} is not divisible by 2")
+    counter += 1
