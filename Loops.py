76f8e36e78d9689e70fa789ec16722185c7d3bfa
ChaosIncarnate7 <natalka.chwalisz@gmail.com>
2025-10-04 20:26:49 +0200

    Learning the difference between loops

--- Loops.py
+++ Loops.py
@@ -1,15 +1,17 @@
-user_count = int(input(How many iterations))
-for x in range (1, user_count):
-    if x % 2 == 0
+user_count = int(input("How many iterations? "))
+for x in range (1, user_count): #[0, 2, 4, 5]: 
+    if x % 2 == 0:
         print(f"number {x} is divisible by 2")
     else:
         print(f"number {x} is not divisible by 2")
 
-user_while_count = int(input("what's the max value?"))
+user_while_count = int(input("what's the max value? "))
 counter = 0
+
 while counter <= user_while_count:
-    if counter % 2 == 0
+    if counter % 2 == 0:
         print(f"number {counter} is divisible by 2")
     else:
         print(f"number {counter} is not divisible by 2")
-    counter += 1
+    counter = counter + 1
+ 
---
+++ task.py
@@ -0,0 +1,5 @@
+user_input = int(input("what's your number of choice?"))
+total_value = 0
+for x in range (user_input + x):
+    total_value = user_input + x
+    print(f"the sum is equal to {total_value}")
