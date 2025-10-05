477e4446e37e749cc426d5f83ea9d7894b973de8
ChaosIncarnate7 <natalka.chwalisz@gmail.com>
2025-10-05 21:18:52 +0200

    Assumed solution

--- task.py
+++ task.py
@@ -1,5 +1,5 @@
 user_input = int(input("what's your number of choice?"))
 total_value = 0
-for x in range (user_input + x):
-    total_value = user_input + x
+for x in range (user_input,20,30):
+    total_value = total_value + x
     print(f"the sum is equal to {total_value}")
