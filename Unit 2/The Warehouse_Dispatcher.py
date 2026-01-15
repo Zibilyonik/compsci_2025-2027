d6dc05df93f07c07aa603e9c330ca75bc1cae99d
SenyaKT <topfloort2019@gmail.com>
2026-01-15 11:35:14 +0100

    Summative done

--- Unit 2/The Warehouse_Dispatcher.py
+++ Unit 2/The Warehouse_Dispatcher.py
@@ -1,63 +1,62 @@
 ### Practice task  the software for an Amazon-style warehouse.
 
+dispatch_queue = []
+shipped_stack = []
 
-Dispatch_Line=[]
-Loa_Bay=[]
-Issue=False
+while True:
+    print("1. Place New Order")
+    print("2. Ship Next Order")
+    print("3. Cancel Last Shipment (Undo)")
+    print("4. View Manifest")
+    print("5. Exit System")
+    choice = input("Select Option: ")
 
-def place_order(order_id):
-    item=input("What item are you shipping today?")
-    ids=[order_id, item] 
-    Dispatch_Line.append(ids)
-    print(f"Order recieved: {ids} is added to queue")
-    
 
-def ship_next():
-    for m in Dispatch_Line:
-        if len(Dispatch_Line)==0:
-            print("No orders are in line")
-            Num_orderID=int(input("Provide ID of the order: "))
+    # 1. PLACE ORDER (Enqueue)
+    if choice == '1':
+        item = input("Enter Order Details: ")
+        dispatch_queue.append(item)
+        print(f"Order received: '{item}'")
+
+
+    # 2. SHIP NEXT (Dequeue -> Push)
+    elif choice == '2':
+        if len(dispatch_queue) == 0:
+            print("No pending orders to ship.")
         else:
-            x=Dispatch_Line[0]
-            Dispatch_Line.pop(0)
-            Loa_Bay.append(x)
-            print(f"Shipped: {x} is loaded onto truck")
-            quality=input(f"Is an order {x} not damaged and has correct adress")
-            if quality=="yes" or quality=="Yes":
-                break
-            else:
-                Issue=True
-                cancel_last_shipment()
-                
-        
+            # FIFO: Remove from index 0(front of line)(simulating dequeue)
+            box = dispatch_queue.pop(0)
+            # LIFO: Add to end (top of stack)(simulating push)
+            shipped_stack.append(box)
+            print(f"Shipped: '{box}' loaded onto truck.")
 
-def cancel_last_shipment():
-    if len(Loa_Bay)==0:
-        ship_next()
+
+    # 3. CANCEL SHIPMENT (Pop -> Enqueue)
+    elif choice == '3':
+        if len(shipped_stack) == 0:
+            print("No shipped orders to cancel.")
+        else:
+            # Remove the most recent item(top of stack)(simulating pop)
+            returned_box = shipped_stack.pop()
+            # Insert at index 0 (Front of line)(simulating enqueue)
+            dispatch_queue.insert(0, returned_box)
+            print(f"Canceled: '{returned_box}' returned to processing.")
+
+
+    # 4. VIEW MANIFEST
+    elif choice == '4':
+        print("To Be Shipped (Queue):", dispatch_queue)
+        print("On The Truck  (Stack):", shipped_stack)
+
+
+    # 5. QUIT
+    elif choice == '5':
+        print("System shutting down...")
+        break
+
+
     else:
-        k=Loa_Bay.pop()
-        Dispatch_Line.insert(0,k)
-        print(f"Shipping Cancelled: {k} returned to the front of line")
-
-
-def view_manifest():
-    problem=0
-    for id in Dispatch_Line:
-            print(f"order {id} is in the Dispatch line")
-    for id in Loa_Bay:
-        print(f"order {id}  is in the Loading Bay ")
-    if Issue==True:
-        problem+=1
-        print(f"There were {problem} issues")
-
-a=True
-while a==True :
-    Num_orderID=input("Provide ID of the order: ")
-    if Num_orderID=="None":
-        a=False
-        ship_next()
-    else:
-        place_order(Num_orderID)
+        print("Invalid command.")
 
 
 
