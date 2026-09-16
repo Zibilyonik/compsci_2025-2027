c04bd15fb2fc6030f18754a9463a3b8a67294ed3
Onur Onat Surmeli <onatsurmeli@gmail.com>
2026-09-16 14:00:31 +0000

    Wed, Sep 16, 2026, 2:00 PM +00:00

--- condts.py
+++ condts.py
@@ -69,3 +69,4 @@ mylist = [14, 15, 17, "hello", True, 7.2] #list (ARRAY in other languages)
           0th,1st,2nd,    3rd,  4th, 5th
 print(mylist)
 7 -> 6   6 -> 5         n -> n - 1
+# write the function calls for printing 2nd, 4th and last item
