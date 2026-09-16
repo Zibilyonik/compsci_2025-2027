20ca2d0a8c48c82e9c8ff32f7f44c3b1cd1cc13d
Onur Onat Surmeli <onatsurmeli@gmail.com>
2026-09-16 13:54:20 +0000

    Wed, Sep 16, 2026, 1:54 PM +00:00

--- condts.py
+++ condts.py
@@ -1,65 +1,69 @@
-# numholder = (257%38)**2//5
-# print(numholder)
-# print("Hello")
-# print("How is life")
-# if numholder > 1000000:
-#     print("It's kinda chill")
-# else:
-#     print("I hate it")
-# if temperature < 18:
-#     grab(hoodie)
-# else:
-#     wear(tshirt)
-# if schedule_includes_polish == True:
-#     grab(notebook)
-# else:
-#     leave(notebook)
-# if time == school_time:
-#     leave()
-# else:
-#     stay()
-# if snow_chance > 80:
-#     ski()
-# else:
-#     chill()
-# if rain == True:
-#     grab(jacket)
-# else:
-#     leave(jacket)
-# if feel == bad:
-#     skip_school()
-# else:
-#     go_school()
-# if first_class == English:
-#     ditch_class()
-# else:
-#     attend()
-# if time == home_time:
-#     leave_school()
-# else:
-#     attend_class()
-# if weather == hot:
-#     put_on(sunglasses)
-# else:
-#     take_off(sunglasses)
-# if rain == True and sun == True:
-#     print("RAINBOW HELL YE")
-# if drink == coke or drink == fanta:
-#     happy()
-# if drink != beetjuice :
-#     happy()
-2 conditions need to be met to listen to music in classroom
-, what keyword to connect these two conditions
-if work_type == alone_work and teacher != talking:
-    listen(music)
-if items == headphones and classroom == empty:
+# # numholder = (257%38)**2//5
+# # print(numholder)
+# # print("Hello")
+# # print("How is life")
+# # if numholder > 1000000:
+# #     print("It's kinda chill")
+# # else:
+# #     print("I hate it")
+# # if temperature < 18:
+# #     grab(hoodie)
+# # else:
+# #     wear(tshirt)
+# # if schedule_includes_polish == True:
+# #     grab(notebook)
+# # else:
+# #     leave(notebook)
+# # if time == school_time:
+# #     leave()
+# # else:
+# #     stay()
+# # if snow_chance > 80:
+# #     ski()
+# # else:
+# #     chill()
+# # if rain == True:
+# #     grab(jacket)
+# # else:
+# #     leave(jacket)
+# # if feel == bad:
+# #     skip_school()
+# # else:
+# #     go_school()
+# # if first_class == English:
+# #     ditch_class()
+# # else:
+# #     attend()
+# # if time == home_time:
+# #     leave_school()
+# # else:
+# #     attend_class()
+# # if weather == hot:
+# #     put_on(sunglasses)
+# # else:
+# #     take_off(sunglasses)
+# # if rain == True and sun == True:
+# #     print("RAINBOW HELL YE")
+# # if drink == coke or drink == fanta:
+# #     happy()
+# # if drink != beetjuice :
+# #     happy()
+# 2 conditions need to be met to listen to music in classroom
+# , what keyword to connect these two conditions
+# if work_type == alone_work and teacher != talking:
+#     listen(music)
+# if items == headphones and classroom == empty:
 
-3 conditions that can allow you to go home early
-, what keyword to connect these three conditions
-if class == cancelled or feel == sick or teacher == missing:
-    go_home_early()
+# 3 conditions that can allow you to go home early
+# , what keyword to connect these three conditions
+# if class == cancelled or feel == sick or teacher == missing:
+#     go_home_early()
 
 x = 15
+print(x)
 soup = "eat"
+print(soup)
 name = "IHSW"
+print(name)
 mylist = [14, 15, 17, "hello", True, 7.2] #list (ARRAY in other languages)
+print(mylist)
