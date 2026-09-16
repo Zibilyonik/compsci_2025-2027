0102bd58f64815153babf539b080b5e325b386a6
Onur Onat Surmeli <onatsurmeli@gmail.com>
2026-09-16 13:51:00 +0000

    Wed, Sep 16, 2026, 1:51 PM +00:00

--- condts.py
+++ condts.py
@@ -62,4 +62,4 @@ if class == cancelled or feel == sick or teacher == missing:
 x = 15
 soup = "eat"
 name = "IHSW"
-mylist = [14, 15, 17, "hello", True, ]
+mylist = [14, 15, 17, "hello", True, 7.2] #list (ARRAY in other languages)
